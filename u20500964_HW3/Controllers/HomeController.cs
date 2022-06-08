6c6abdd44860511148c4e5b904379657becc3de7
u20500964 <u20500964@tuks.co.za>
2022-06-08 14:54:48 +0200

    Add project files.

---
+++ u20500964_HW3/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace u20500964_HW3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string FileType, HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    if (FileType == "Image")
+                    {
+                        string _FileName = Path.GetFileName(file.FileName);
+                        string _path = Path.Combine(Server.MapPath("~/Media/Images/"), _FileName);
+                        file.SaveAs(_path);
+                    }
+
+                    if (FileType == "Video")
+                    {
+                        string _FileName = Path.GetFileName(file.FileName);
+                        string _path = Path.Combine(Server.MapPath("~/Media/Videos/"), _FileName);
+                        file.SaveAs(_path);
+                    }
+                    if (FileType == "Document")
+                    {
+                        string _FileName = Path.GetFileName(file.FileName);
+                        string _path = Path.Combine(Server.MapPath("~/Media/Documents/"), _FileName);
+                        file.SaveAs(_path);
+                    }
+                }
+                ViewBag.Message = "File Uploaded Successfully!!";
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "File upload failed!!";
+                return View();
+            }
+            return View();
+        }
+
+
+    }
+}
---
+++ u20500964_HW3/Controllers/ImagesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using u20500964_HW3.Models;
+
+namespace u20500964_HW3.Controllers
+{
+    public class ImagesController : Controller
+    {
+        // GET: Files
+        public ActionResult Index()
+        {
+            // 
+            //Fetch all files in the Folder (Directory).
+            string[] filePaths = Directory.GetFiles(Server.MapPath("~/Media/Images/"));
+
+            //Copy File names to Model collection.
+            List<FileModel> files = new List<FileModel>();
+            foreach (string filePath in filePaths)
+            {
+                files.Add(new FileModel { FileName = Path.GetFileName(filePath) });
+            }
+
+            return View(files);
+
+        }
+
+        public FileResult Download(string FileName)
+        {
+            //Build the File Path.
+            string path = Server.MapPath("~/Media/Images/") + FileName;
+
+            //Read the File data into Byte Array.
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+
+            //Send the File to Download.
+            return File(bytes, "application/octet-stream", FileName);
+        }
+
+
+        public ActionResult Delete(string FileName)
+        {
+            string fullPath = Request.MapPath("~/Media/Images/" + FileName);
+            if (System.IO.File.Exists(fullPath))
+            {
+                System.IO.File.Delete(fullPath);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
